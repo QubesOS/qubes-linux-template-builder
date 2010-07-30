0acdee8144642bec3051b2b6ea0236b0acf81556
Joanna Rutkowska <joanna@invisiblethingslab.com>
2010-07-30 16:42:17 +0200

    dispvm-prerun.sh minor updates

--- vm_conf_files/dispvm-prerun.sh
+++ vm_conf_files/dispvm-prerun.sh
@@ -2,17 +2,18 @@
 
 apps="evince okular openoffice gwenview firefox"
 
+cat /etc/dispvm-dotfiles.tbz | tar -xjf- --overwrite -C /home/user --owner user 2>&1 >/tmp/dispvm-dotfiles-errors.log
+
 for app in $apps ; do
     echo "Launching: $app..."
     $app >/tmp/dispvm_prerun_errors.log 2>&1 &
 done
 
 echo "Sleeping..."
-sleep 120
+sleep 60
 
 ps ax > /tmp/dispvm-prerun-proclist.log
 
 cat /etc/dispvm-dotfiles.tbz | tar -xjf- --overwrite -C /home/user --owner user 2>&1 >/tmp/dispvm-dotfiles-errors.log
 
 echo done.
-
