2ed5b9b2468fd04a9bdaae391b36f9d07cb0091f
Joanna Rutkowska <joanna@invisiblethingslab.com>
2011-07-30 13:07:28 +0200

    Update flashplayer to v11 Beta 1

--- add_3rd_party_software.sh
+++ add_3rd_party_software.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-cp rpms_3rd_party/libflashplayer.so mnt/usr/lib64/mozilla/plugins/
+cp 3rd_party_software/libflashplayer.so mnt/usr/lib64/mozilla/plugins/
+cp 3rd_party_software/flash-player-properties mnt/usr/bin/
