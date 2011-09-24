179c6583486415fad0b99853cc79f1f5bd33aec7
Marek Marczykowski <marmarek@invisiblethingslab.com>
2011-09-24 11:58:14 +0200

    Easy switch of base distro for template ($DIST env var)

--- create_symlinks_in_rpms_to_install_dir.sh
+++ create_symlinks_in_rpms_to_install_dir.sh
@@ -3,15 +3,17 @@
 SRC_ROOT=../..
 [ -n "$1" ] && SRC_ROOT=$1
 
+: DIST=fc14
+
 rm -fr rpms_to_install/*
-VERSION_CORE=$(cat version_core)
+VERSION_CORE=$(cat version_core | sed "s/DIST/$DIST/")
 ln -s $SRC_ROOT/core/rpm/x86_64/qubes-core-appvm-$VERSION_CORE.rpm rpms_to_install/qubes-core-appvm
 ln -s $SRC_ROOT/core/rpm/x86_64/qubes-core-appvm-libs-$VERSION_CORE.rpm rpms_to_install/qubes-core-appvm-libs
 ln -s $SRC_ROOT/core/rpm/x86_64/qubes-core-commonvm-$VERSION_CORE.rpm rpms_to_install/qubes-core-commonvm
 ln -s $SRC_ROOT/core/rpm/x86_64/qubes-core-netvm-$VERSION_CORE.rpm rpms_to_install/qubes-core-netvm
 ln -s $SRC_ROOT/core/rpm/x86_64/qubes-core-proxyvm-$VERSION_CORE.rpm rpms_to_install/qubes-core-proxyvm
 
-VERSION_GUI=$(cat version_gui)
+VERSION_GUI=$(cat version_gui | sed "s/DIST/$DIST/")
 ln -s $SRC_ROOT/gui/rpm/x86_64/qubes-gui-vm-$VERSION_GUI.rpm rpms_to_install/qubes-gui-vm
 
 VERSION_XEN=$(cat version_xen)
