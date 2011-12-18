61f9c5048fe26ffb15c5b38b322546b26b7e4fe3
Marek Marczykowski <marmarek@invisiblethingslab.com>
2011-12-18 13:42:57 +0100

    Install addons by default

--- create_symlinks_in_rpms_to_install_dir.sh
+++ create_symlinks_in_rpms_to_install_dir.sh
@@ -20,3 +20,9 @@ VERSION_XEN=$(cat version_xen)
 ln -s $SRC_ROOT/xen/rpm/x86_64/xen-libs-$VERSION_XEN.rpm rpms_to_install/xen-libs
 ln -s $SRC_ROOT/xen/rpm/x86_64/xen-licenses-$VERSION_XEN.rpm rpms_to_install/xen-licenses
 ln -s $SRC_ROOT/xen/rpm/x86_64/xen-qubes-vm-essentials-$VERSION_XEN.rpm rpms_to_install/xen-qubes-vm-essentials
+
+# Install also addons
+VERSION_TB=$(cat version_addon_tb | sed "s/%DIST%/$DIST/")
+ln -s $SRC_ROOT/addons/rpm/x86_64/thunderbird-qubes-$VERSION_TB.rpm rpms_to_install/thunderbird-qubes
+VERSION_GPG=$(cat version_addon_gpg | sed "s/%DIST%/$DIST/")
+ln -s $SRC_ROOT/addons/rpm/x86_64/qubes-gpg-split-$VERSION_GPG.rpm rpms_to_install/qubes-gpg-split
