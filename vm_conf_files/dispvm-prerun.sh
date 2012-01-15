30301440c47b77a7edfd3987cb4e7a804358eb05
Marek Marczykowski <marmarek@invisiblethingslab.com>
2012-01-15 04:41:51 +0100

    dispvm: append to prerun logs instead of overriding them

--- vm_conf_files/dispvm-prerun.sh
+++ vm_conf_files/dispvm-prerun.sh
@@ -6,7 +6,7 @@ cat /etc/dispvm-dotfiles.tbz | tar -xjf- --overwrite -C /home/user --owner user 
 
 for app in $apps ; do
     echo "Launching: $app..."
-    $app >/tmp/dispvm_prerun_errors.log 2>&1 &
+    $app >>/tmp/dispvm_prerun_errors.log 2>&1 &
 done
 
 echo "Sleeping..."
@@ -14,6 +14,6 @@ sleep 60
 
 ps ax > /tmp/dispvm-prerun-proclist.log
 
-cat /etc/dispvm-dotfiles.tbz | tar -xjf- --overwrite -C /home/user --owner user 2>&1 >/tmp/dispvm-dotfiles-errors.log
+cat /etc/dispvm-dotfiles.tbz | tar -xjf- --overwrite -C /home/user --owner user 2>&1 >>/tmp/dispvm-dotfiles-errors.log
 
 echo done.
