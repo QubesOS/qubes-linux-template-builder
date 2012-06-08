64e6b25843838f4815202f6d4be31d140a76eca7
Marek Marczykowski <marmarek@invisiblethingslab.com>
2012-06-08 16:51:14 +0200

    rpms_symlinks: automatically obtain components versions

--- create_symlinks_in_rpms_to_install_dir.sh
+++ create_symlinks_in_rpms_to_install_dir.sh
@@ -7,7 +7,8 @@ SRC_ROOT=../..
 
 rm -fr rpms_to_install/*
 pushd rpms_to_install
-VERSION_CORE=$(cat version_core | sed "s/%DIST%/$DIST/")
+# FIXME: rel hardcoded
+VERSION_CORE=$(cat $SRC_ROOT/core/version_vm)-1.$DIST.x86_64
 ln -s $SRC_ROOT/core/rpm/x86_64/qubes-core-vm-$VERSION_CORE.rpm qubes-core-vm
 ln -s $SRC_ROOT/core/rpm/x86_64/qubes-core-vm-libs-$VERSION_CORE.rpm qubes-core-vm-libs
 if [ "${DIST/fc/}" -ge 15 ]; then
@@ -16,18 +17,21 @@ else
     ln -s $SRC_ROOT/core/rpm/x86_64/qubes-core-vm-sysvinit-$VERSION_CORE.rpm qubes-core-vm-init
 fi
 
-VERSION_GUI=$(cat version_gui | sed "s/%DIST%/$DIST/")
+# FIXME: rel hardcoded
+VERSION_GUI=$(cat $SRC_ROOT/gui/version)-1.$DIST.x86_64
 ln -s $SRC_ROOT/gui/rpm/x86_64/qubes-gui-vm-$VERSION_GUI.rpm qubes-gui-vm
 
-VERSION_XEN=$(cat version_xen)
+VERSION_XEN=$(cat $SRC_ROOT/xen/version)-$(cat $SRC_ROOT/xen/rel).qubes.x86_64
 ln -s $SRC_ROOT/xen/rpm/x86_64/xen-libs-$VERSION_XEN.rpm xen-libs
 ln -s $SRC_ROOT/xen/rpm/x86_64/xen-licenses-$VERSION_XEN.rpm xen-licenses
 ln -s $SRC_ROOT/xen/rpm/x86_64/xen-qubes-vm-essentials-$VERSION_XEN.rpm xen-qubes-vm-essentials
 
 # Install also addons
-VERSION_TB=$(cat version_addon_tb | sed "s/%DIST%/$DIST/")
+# FIXME: rel hardcoded
+VERSION_TB=$(cat $SRC_ROOT/addons/thunderbird-qubes/version)-1.$DIST.x86_64
 ln -s $SRC_ROOT/addons/rpm/x86_64/thunderbird-qubes-$VERSION_TB.rpm thunderbird-qubes
-VERSION_GPG=$(cat version_addon_gpg | sed "s/%DIST%/$DIST/")
+# FIXME: rel hardcoded
+VERSION_GPG=$(cat $SRC_ROOT/addons/gpg-split/version)-1.$DIST.x86_64
 ln -s $SRC_ROOT/addons/rpm/x86_64/qubes-gpg-split-$VERSION_GPG.rpm qubes-gpg-split
 
 popd
