8a1a1ac34d3c0f11428873d4c3fb5cd85c6ed469
Marek Marczykowski <marmarek@invisiblethingslab.com>
2012-06-08 16:48:38 +0200

    rpms_symlinks: enter rpms_to_install dir before creating
    Now symlinks are created from dest dir, which will simplify relavite paths.

--- create_symlinks_in_rpms_to_install_dir.sh
+++ create_symlinks_in_rpms_to_install_dir.sh
@@ -6,25 +6,28 @@ SRC_ROOT=../..
 : DIST=fc14
 
 rm -fr rpms_to_install/*
+pushd rpms_to_install
 VERSION_CORE=$(cat version_core | sed "s/%DIST%/$DIST/")
-ln -s $SRC_ROOT/core/rpm/x86_64/qubes-core-vm-$VERSION_CORE.rpm rpms_to_install/qubes-core-vm
-ln -s $SRC_ROOT/core/rpm/x86_64/qubes-core-vm-libs-$VERSION_CORE.rpm rpms_to_install/qubes-core-vm-libs
+ln -s $SRC_ROOT/core/rpm/x86_64/qubes-core-vm-$VERSION_CORE.rpm qubes-core-vm
+ln -s $SRC_ROOT/core/rpm/x86_64/qubes-core-vm-libs-$VERSION_CORE.rpm qubes-core-vm-libs
 if [ "${DIST/fc/}" -ge 15 ]; then
-    ln -s $SRC_ROOT/core/rpm/x86_64/qubes-core-vm-systemd-$VERSION_CORE.rpm rpms_to_install/qubes-core-vm-init
+    ln -s $SRC_ROOT/core/rpm/x86_64/qubes-core-vm-systemd-$VERSION_CORE.rpm qubes-core-vm-init
 else
-    ln -s $SRC_ROOT/core/rpm/x86_64/qubes-core-vm-sysvinit-$VERSION_CORE.rpm rpms_to_install/qubes-core-vm-init
+    ln -s $SRC_ROOT/core/rpm/x86_64/qubes-core-vm-sysvinit-$VERSION_CORE.rpm qubes-core-vm-init
 fi
 
 VERSION_GUI=$(cat version_gui | sed "s/%DIST%/$DIST/")
-ln -s $SRC_ROOT/gui/rpm/x86_64/qubes-gui-vm-$VERSION_GUI.rpm rpms_to_install/qubes-gui-vm
+ln -s $SRC_ROOT/gui/rpm/x86_64/qubes-gui-vm-$VERSION_GUI.rpm qubes-gui-vm
 
 VERSION_XEN=$(cat version_xen)
-ln -s $SRC_ROOT/xen/rpm/x86_64/xen-libs-$VERSION_XEN.rpm rpms_to_install/xen-libs
-ln -s $SRC_ROOT/xen/rpm/x86_64/xen-licenses-$VERSION_XEN.rpm rpms_to_install/xen-licenses
-ln -s $SRC_ROOT/xen/rpm/x86_64/xen-qubes-vm-essentials-$VERSION_XEN.rpm rpms_to_install/xen-qubes-vm-essentials
+ln -s $SRC_ROOT/xen/rpm/x86_64/xen-libs-$VERSION_XEN.rpm xen-libs
+ln -s $SRC_ROOT/xen/rpm/x86_64/xen-licenses-$VERSION_XEN.rpm xen-licenses
+ln -s $SRC_ROOT/xen/rpm/x86_64/xen-qubes-vm-essentials-$VERSION_XEN.rpm xen-qubes-vm-essentials
 
 # Install also addons
 VERSION_TB=$(cat version_addon_tb | sed "s/%DIST%/$DIST/")
-ln -s $SRC_ROOT/addons/rpm/x86_64/thunderbird-qubes-$VERSION_TB.rpm rpms_to_install/thunderbird-qubes
+ln -s $SRC_ROOT/addons/rpm/x86_64/thunderbird-qubes-$VERSION_TB.rpm thunderbird-qubes
 VERSION_GPG=$(cat version_addon_gpg | sed "s/%DIST%/$DIST/")
-ln -s $SRC_ROOT/addons/rpm/x86_64/qubes-gpg-split-$VERSION_GPG.rpm rpms_to_install/qubes-gpg-split
+ln -s $SRC_ROOT/addons/rpm/x86_64/qubes-gpg-split-$VERSION_GPG.rpm qubes-gpg-split
+
+popd
