2478ad1b9dd07b23a815c5299b539eaa27a97e70
Olivier Medoc <o_medoc@yahoo.fr>
2013-02-08 15:31:19 +0100

    scripts_archlinux: implemented iso file verification

--- scripts_archlinux/00_prepare.sh
+++ scripts_archlinux/00_prepare.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
+ISO_VERSION=2013.02.01
+
 
 echo "Downloading Archlinux dvd..."
-wget -O "archlinux.iso" "http://mir.archlinux.fr/iso/latest/arch/x86_64/root-image.fs.sfs" --continue
+wget -O "archlinux-$ISO_VERSION-dual.iso" "http://mir.archlinux.fr/iso/$ISO_VERSION/archlinux-$ISO_VERSION-dual.iso" --continue
+
+echo "Verifying dvd..."
+echo "If verification fails, ensure that you imported and verified the archlinux key"
+echo "eg: gpg --recv-keys 9741E8AC"
+
+gpg --verify "./scripts_archlinux/archlinux-$ISO_VERSION-dual.iso.sig" "archlinux-$ISO_VERSION-dual.iso" || exit
 
 echo "Extracting squash filesystem from DVD..."
 mkdir archlinux_dvd
-sudo mount -o loop archlinux.iso archlinux_dvd
+sudo mount -o loop "archlinux-$ISO_VERSION-dual.iso" archlinux_dvd
+cp archlinux_dvd/arch/x86_64/root-image.fs.sfs .
+sudo umount archlinux_dvd
+sudo mount -o loop root-image.fs.sfs archlinux_dvd
 cp archlinux_dvd/root-image.fs .
 sudo umount archlinux_dvd
+rm root-image.fs.sfs
