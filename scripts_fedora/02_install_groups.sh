26663e2a5812a2d87f8992636d587b02fe4ce68d
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2014-05-13 04:30:01 +0200

    fedora: Improve error handling

--- scripts_fedora/02_install_groups.sh
+++ scripts_fedora/02_install_groups.sh
@@ -10,7 +10,7 @@ fi
 export PKGGROUPS=$(cat $PKGLISTFILE)
 
 export YUM0=$PWD/yum_repo_qubes
-yum clean all -c $PWD/yum.conf $YUM_OPTS -y --installroot=$PWD/mnt
+yum clean all -c $PWD/yum.conf $YUM_OPTS -y --installroot=$INSTALLDIR
 yum install -c $PWD/yum.conf $YUM_OPTS -y --installroot=$INSTALLDIR $PKGGROUPS || RETCODE=1
 yum update -c $PWD/yum.conf $YUM_OPTS -y --installroot=$INSTALLDIR || RETCODE=1
 
