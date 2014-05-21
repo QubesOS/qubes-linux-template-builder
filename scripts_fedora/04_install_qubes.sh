32ed8052bf56bfd2b09655c2500610a82d5f55be
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2014-05-21 18:58:58 +0200

    fedora: improve error handling
    Make sure that all partitions got unmounted.

--- scripts_fedora/04_install_qubes.sh
+++ scripts_fedora/04_install_qubes.sh
@@ -2,12 +2,14 @@
 echo "--> Preparing environment..."
 mount -t proc proc $PWD/mnt/proc
 
+trap "umount $PWD/mnt/proc" EXIT
+
+export YUM0=$PWD/yum_repo_qubes
 if [ "$TEMPLATE_FLAVOR" == "minimal" ]; then
     YUM_OPTS="$YUM_OPTS --group_package_types=mandatory"
 fi
 
 echo "--> Installing RPMs..."
-export YUM0=$PWD/yum_repo_qubes
 yum install -c $PWD/yum.conf $YUM_OPTS -y --installroot=$(pwd)/mnt @qubes-vm || RETCODE=1
 
 rpm --root=$PWD/mnt --import $PWD/mnt/etc/pki/rpm-gpg/RPM-GPG-KEY-qubes-*
@@ -16,7 +18,5 @@ if [ "$TEMPLATE_FLAVOR" != "minimal" ]; then
     echo "--> Installing 3rd party apps"
     $SCRIPTSDIR/add_3rd_party_software.sh || RETCODE=1
 fi
-
-umount $PWD/mnt/proc
 
 exit $RETCODE
