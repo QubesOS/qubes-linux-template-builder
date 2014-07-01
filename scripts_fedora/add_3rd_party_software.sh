634ddcb7647aed0fbf4a05cbd293f90e52f73742
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2014-07-02 01:05:21 +0200

    fedora: disable adobe repo in default template (#828)

--- scripts_fedora/add_3rd_party_software.sh
+++ scripts_fedora/add_3rd_party_software.sh
@@ -3,5 +3,9 @@
 INSTALLDIR=$PWD/mnt
 
 rpm -i --root=$INSTALLDIR $SCRIPTSDIR/3rd_party_software/adobe-release-x86_64-*.noarch.rpm || exit 1
-rpm --import --root=$INSTALLDIR mnt/etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
-yum install -c $PWD/yum.conf $YUM_OPTS -y --installroot=$INSTALLDIR flash-plugin || exit 1
+if [ "$TEMPLATE_FLAVOR" == "fullyloaded" ]; then
+    rpm --import --root=$INSTALLDIR mnt/etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
+    yum install -c $PWD/yum.conf $YUM_OPTS -y --installroot=$INSTALLDIR flash-plugin || exit 1
+else
+    yum-config-manager --installroot=$INSTALLDIR --disable adobe-linux-x86_64 > /dev/null
+fi
